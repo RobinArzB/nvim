f461b94c4689211ad529db2ca4a8c279917fcc28
robinho <robinho@pop-os.home>
2024-01-20 13:00:44 +0100

    fix colorscheme and init

--- .config/nvim/lua/robinho/lazy/colorscheme.lua
+++ .config/nvim/lua/robinho/lazy/colorscheme.lua
@@ -7,20 +7,34 @@ function ColorMyPencils(color)
 end
 
 return {
+  {
     "folke/tokyonight.nvim",
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-            require('rose-pine').setup({
-                styles = {
-                    italic = false,
-                    transparency = true,
-                },
-            })
-            ColorMyPencils()
-        end
-    },
+    config = function()
+      require("tokyonight").setup({
+        style = "storm",
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          sidebars = "dark",
+          floats = "dark",
+        },
+      })
+    end
+  },
+
+  {
+    "rose-pine/neovim",
+    as = "rose-pine",
+    config = function()
+      require('rose-pine').setup({
+        trasnparency = true,
+      })
+      ColorMyPencils()
+    end
+  },
+
+
 }
 
